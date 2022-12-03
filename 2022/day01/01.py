8ba6f8bf378316d7a41abb28d510a0a83c6d7b73
YousraMashkoor <yousra.mashkoor@gmail.com>
2022-12-03 09:41:33 +0500

    2022-day01 improved

--- 2022/day01/01.py
+++ 2022/day01/01.py
@@ -1,36 +1,53 @@
-p1 = 0
-p2 = 0
-
-max = 0
-count = 0
-max_list = []
+## improved solution
 
 
-with open('input.txt', 'a') as f:
-        f.write('\n')
-for x in open('input.txt'):
-    if x != '\n':
-        x = int(x)
-        # breakpoint()
-        count = count + x
-    else:
-        if len(max_list)<3:
-            max_list.append(count)
-            max_list.sort()
-            # print(max_list)
-        else:
-            if any(count> i for i in max_list):
-                # if count == 10000:
-                for i,it in enumerate(max_list):
-                    if it<count:
-                        max_list[i] = count
-                        max_list.sort()
-                        break
 
-        if count>max:
-            max = count
-        count = 0
-print(max)
-print(max_list)
-print(sum(max_list))
+X = [x.strip() for x in open('input.txt')]
+Q = []
+for elf in ('\n'.join(X).split('\n\n')):
+    q = 0
+    for x in elf.split('\n'):
+        q += int(x)
+    Q.append(q)
+
+Q.sort()
+print(Q[-1])
+print(Q[-1]+Q[-2]+Q[-3])
+
+
+
+
+
+# max = 0
+# count = 0
+# max_list = []
+
+
+# with open('input.txt', 'a') as f:
+#         f.write('\n')
+# for x in open('input.txt'):
+#     if x != '\n':
+#         x = int(x)
+#         # breakpoint()
+#         count = count + x
+#     else:
+#         if len(max_list)<3:
+#             max_list.append(count)
+#             max_list.sort()
+#             # print(max_list)
+#         else:
+#             if any(count> i for i in max_list):
+#                 # if count == 10000:
+#                 for i,it in enumerate(max_list):
+#                     if it<count:
+#                         max_list[i] = count
+#                         max_list.sort()
+#                         break
+
+#         if count>max:
+#             max = count
+#         count = 0
+# print(max)
+# print(max_list)
+# print(sum(max_list))
 
